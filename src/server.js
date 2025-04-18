f676bd9897f47e49e0e911521ea9cc32c1d37b77
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-04-18 20:52:19 +0100

    Added two middlewares

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import dotenv from "dotenv";
+import router from "./routes/route.js";
 
 dotenv.config();
 
@@ -24,6 +25,33 @@ app.use(
     },
   })
 );
+
+// Block direct access to /admin and /admin/delete
+app.use((req, res, next) => {
+  const originalUrl = req.originalUrl;
+  const xOriginalUrl = req.headers["x-original-url"];
+
+  // If request is direct to /admin or /admin/delete and doesn't include X-Original-URL
+  if (
+    (originalUrl === "/admin" || originalUrl.startsWith("/admin/delete")) &&
+    !xOriginalUrl
+  ) {
+    return res.status(403).send("Access to /admin is blocked by front-end.");
+  }
+
+  next();
+});
+
+// Check if X-Original-URL is present and serve the spoofed route
+app.use((req, res, next) => {
+  const spoofedUrl = req.headers["x-original-url"];
+  if (spoofedUrl) {
+    req.url = spoofedUrl;
+  }
+  next();
+});
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log("Servers is running on port " + PORT);
