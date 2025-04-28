ab7c0b0028c9f9fc7637f1ccc6eb245e385723ef
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-04-28 22:25:09 +0100

    Updated

--- src/controllers/deleteUser.js
+++ src/controllers/deleteUser.js
@@ -1,19 +1,20 @@
-// In-memory user storage
-let users = [
-  { username: 'wiener' },
-  { username: 'carlos' }
-];
+let users = [{ username: "wiener" }, { username: "carlos" }];
 
 export const deleteUser = (req, res) => {
   const username = req.query.username;
+
+  const isExit = users.find((user) => user.username === username);
+  if (!isExit) {
+    return res.status(404).send("User not found.");
+  }
 
   if (!username) {
     return res.status(400).send("Missing username.");
   }
 
   // Remove the user
-  users = users.filter(user => user.username !== username);
-  
+  users = users.filter((user) => user.username !== username);
+
   console.log(`Deleted user: ${username}`);
   res.send(`<p>User <strong>${username}</strong> has been deleted.</p>`);
 };
